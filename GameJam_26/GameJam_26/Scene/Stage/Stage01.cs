01e610d75bc9daac746b3760a707952f97ac23d9
AL-XQ <thealienxq@gmail.com>
2018-09-06 13:46:04 +0900

    sute-ji

--- GameJam_26/GameJam_26/Scene/Stage/Stage01.cs
+++ GameJam_26/GameJam_26/Scene/Stage/Stage01.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InfinityGame.GameGraphics;
+using InfinityGame.Device;
+
+using InfinityGame.Stage.StageObject;
 using Microsoft.Xna.Framework.Graphics;
+using InfinityGame.Element;
 
 namespace GameJam_26.Scene.Stage
 {
@@ -13,6 +17,17 @@ namespace GameJam_26.Scene.Stage
         public Stage01(GraphicsDevice aGraphicsDevice, BaseDisplay aParent, string aName) : base(aGraphicsDevice, aParent, aName)
         {
 
+        }
+
+        public override void Initialize()
+        {
+            
+            base.Initialize();
+        }
+
+        public override void PreLoadContent()
+        {
+            base.PreLoadContent();
         }
     }
 }
