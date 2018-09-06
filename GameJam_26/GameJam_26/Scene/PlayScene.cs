33dc33ae160bc177571458159a9ff8977f1766a5
GS2組mitsuhashi <hirokurospeed@gmail.com>
2018-09-06 15:17:51 +0900

    playscene

--- GameJam_26/GameJam_26/Scene/PlayScene.cs
+++ GameJam_26/GameJam_26/Scene/PlayScene.cs
@@ -9,11 +9,19 @@ using InfinityGame.Scene;
 using Microsoft.Xna.Framework.Graphics;
 
 using GameJam_26.Scene.Stage;
-
+using InfinityGame.UI;
+using InfinityGame.UI.UIContent;
+using Microsoft.Xna.Framework;
+using InfinityGame.Element;
+using GameJam_26.Scene.UI;
 namespace GameJam_26.Scene
 {
     public class PlayScene : StageScene
     {
+        private UIWindow ending;
+        private BackMenu backMenu;
+
+       
         public PlayScene(string aName, GraphicsDevice aGraphicsDevice, BaseDisplay aParent, GameRun aGameRun) : base(aName, aGraphicsDevice, aParent, aGameRun)
         {
 
@@ -27,7 +35,19 @@ namespace GameJam_26.Scene
         public override void PreLoadContent()
         {
             new Stage01(GraphicsDevice, this, "Stage01");
+            ending = new UIWindow(graphicsDevice, this);
+            ending.Size = new Size(size.Width * 4 / 5, size.Height * 2 / 3);
+            ending.Location = (size / 2 - backMenu.Size / 2).ToPoint();
+            backMenu = new BackMenu(graphicsDevice, this);
+            backMenu.Size = new Size(size.Width/5,size.Height/2);
+            backMenu.Location = (size / 2 - backMenu.Size / 2).ToPoint();
             base.PreLoadContent();
         }
+        public override void LoadContent()
+        {
+            backMenu.Text = GetText("backMenu");
+            base.LoadContent();
+        }
+
     }
 }
---
+++ GameJam_26/GameJam_26/Scene/UI/BackMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityGame.UI.UIContent;
+using InfinityGame.UI;
+using InfinityGame.GameGraphics;
+using Microsoft.Xna.Framework.Graphics;
+using InfinityGame.Element;
+using Microsoft.Xna.Framework;
+
+namespace GameJam_26.Scene.UI
+{
+    public class BackMenu : UIWindow
+    {
+        private AnimeButton back;
+        private AnimeButton title;
+        private AnimeButton exit;
+        public BackMenu(GraphicsDevice aGraphicsDevice, BaseDisplay parent) : base(aGraphicsDevice, parent)
+        {
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+        public override void PreLoadContent()
+        {
+            back = new AnimeButton(graphicsDevice, this);
+            title = new AnimeButton(graphicsDevice, this);
+            exit = new AnimeButton(graphicsDevice, this);
+            back.Size = new Size(250, 75);
+            title.Size = new Size(250, 75);
+            exit.Size = new Size(250, 75);
+            back.Location = new Point(10, 10);
+            title.Location = new Point(10, 95);
+            exit.Location = new Point(10, 180);
+            base.PreLoadContent();
+        }
+        public override void LoadContent()
+        {
+            back.Text = GetText("back");
+            title.Text = GetText("title");
+            exit.Text = GetText("exit");
+            back.TextAlign = ContentAlignment.MiddleCenter;
+            title.TextAlign = ContentAlignment.MiddleCenter;
+            exit.TextAlign = ContentAlignment.MiddleCenter;
+
+            base.LoadContent();
+        }
+    }
+}
