65d5355e5109c56ca3ea256c293b85b5a7ce0189
AL-XQ <thealienxq@gmail.com>
2018-09-07 22:48:59 +0900

    ツリー修正01

---
+++ GameJam_26/GameJam_26/Scene/Stage/BaseItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfinityGame.GameGraphics;
+using InfinityGame.Stage.StageObject;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameJam_26.Scene.Stage
+{
+    public class BaseItem : StageObj
+    {
+        public BaseItem(GraphicsDevice aGraphicsDevice, BaseDisplay aParent, string aName) : base(aGraphicsDevice, aParent, aName)
+        {
+
+        }
+    }
+}
